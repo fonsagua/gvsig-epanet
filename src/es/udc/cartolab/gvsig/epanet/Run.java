738fe6ceaea6d9d54478dbfa75628694e14f98ef
Francisco Puga <fpuga@cartolab.es>
2013-07-12 12:58:10 +0200

    Adds empty extension for gvSIG

---
+++ src/es/udc/cartolab/gvsig/epanet/Run.java
@@ -0,0 +1,28 @@
+package es.udc.cartolab.gvsig.epanet;
+
+
+public class Run extends BasicExtension {
+
+    @Override
+    public void initialize() {
+	registerIcon("run_epanet");
+
+    }
+
+    @Override
+    public void execute(String actionCommand) {
+	Foo foo = new Foo();
+	foo.execute();
+    }
+
+    @Override
+    public boolean isEnabled() {
+	return true;
+    }
+
+    @Override
+    public boolean isVisible() {
+	return true;
+    }
+
+}
