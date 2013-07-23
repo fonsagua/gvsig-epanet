24a668505e63591cb4c4edf1b2ad7218da8f729e
Jorge López <jlopez@cartolab.es>
2013-07-23 19:21:47 +0200

    EndGeometryListener fixed.
    It was not properly removing the CAD listeners
    and setting a non-CAD tool.

--- src/es/udc/cartolab/gvsig/epanet/JunctionExtension.java
+++ src/es/udc/cartolab/gvsig/epanet/JunctionExtension.java
@@ -1,10 +1,18 @@
 package es.udc.cartolab.gvsig.epanet;
 
-import com.iver.cit.gvsig.exceptions.visitors.StopWriterVisitorException;
+import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
+import com.iver.andami.PluginServices;
+import com.iver.andami.messages.NotificationManager;
+import com.iver.cit.gvsig.CADExtension;
+import com.iver.cit.gvsig.EditionManager;
 import com.iver.cit.gvsig.fmap.layers.FLayer;
+import com.iver.cit.gvsig.fmap.layers.FLyrAnnotation;
+import com.iver.cit.gvsig.fmap.layers.FLyrVect;
 import com.iver.cit.gvsig.gui.cad.tools.PointCADTool;
+import com.iver.cit.gvsig.layers.VectorialLayerEdited;
 import com.iver.cit.gvsig.listeners.CADListenerManager;
 import com.iver.cit.gvsig.listeners.EndGeometryListener;
+import com.iver.cit.gvsig.project.documents.view.gui.View;
 
 import es.udc.cartolab.gvsig.epanet.config.Preferences;
 import es.udc.cartolab.gvsig.navtable.ToggleEditing;
@@ -26,14 +34,36 @@ public class JunctionExtension extends AbstractCADExtension {
 
 	@Override
 	public void endGeometry(FLayer layer, String cadToolKey) {
-
-	    ToggleEditing toggleEditing = new ToggleEditing();
-	    try {
-		toggleEditing.stopEditing(layer, false);
-	    } catch (StopWriterVisitorException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	    View vista = (View) PluginServices.getMDIManager()
+		    .getActiveWindow();
+	    EditionManager edMan = CADExtension.getEditionManager();
+	    vista.getMapControl().getCanceldraw().setCanceled(true);
+	    if (layer instanceof FLyrVect) {
+		FLyrVect lv = (FLyrVect) layer;
+		VectorialLayerEdited lyrEd = (VectorialLayerEdited) edMan
+			.getActiveLayerEdited();
+		try {
+		    lv.getRecordset().removeSelectionListener(lyrEd);
+		} catch (ReadDriverException e) {
+		    NotificationManager
+			    .addError("Remove Selection Listener", e);
+		}
+		try {
+		    new ToggleEditing().stopEditing(layer, false);
+		    lv.removeLayerListener(edMan);
+		    if (lv instanceof FLyrAnnotation) {
+			FLyrAnnotation lva = (FLyrAnnotation) lv;
+			lva.setMapping(lva.getMapping());
+		    }
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
 	    }
+	    vista.getMapControl().setTool("zoomIn");
+	    vista.hideConsole();
+	    vista.repaintMap();
+	    CADExtension.clearView();
+	    PluginServices.getMainFrame().enableControls();
 	}
     }
 }
