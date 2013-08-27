5d09bd27f07c29f3084c323a9d5cd9dc6532d616
Francisco Puga <fpuga@cartolab.es>
2013-08-27 18:10:51 +0200

    Set foreign values, like 'cod_alternativa' automatically when using the cad tools

---
+++ src/es/udc/cartolab/gvsig/epanet/config/FonsaguaAlternative.java
@@ -0,0 +1,6 @@
+package es.udc.cartolab.gvsig.epanet.config;
+
+public class FonsaguaAlternative {
+    public static String code = "";
+    public static String alternativePK = "cod_alternativa";
+}
