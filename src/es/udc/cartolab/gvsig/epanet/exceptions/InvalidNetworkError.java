5a29aaae78df72d297f2d4008b282f261a7588ab
Francisco Puga <fpuga@cartolab.es>
2013-09-20 11:57:29 +0200

    Allow set the error id in the InvalidNetworkError constructor

--- src/es/udc/cartolab/gvsig/epanet/exceptions/InvalidNetworkError.java
+++ src/es/udc/cartolab/gvsig/epanet/exceptions/InvalidNetworkError.java
@@ -3,6 +3,13 @@ package es.udc.cartolab.gvsig.epanet.exceptions;
 @SuppressWarnings("serial")
 public class InvalidNetworkError extends Exception {
 
+    private int id;
+
+    public InvalidNetworkError(int id, String msg) {
+	super(msg);
+	this.id = id;
+    }
+
     public InvalidNetworkError(String msg) {
 	super(msg);
     }
